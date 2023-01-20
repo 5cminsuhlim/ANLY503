d0d45967e4bee19cf45a4f1421487f01af8b8326
Eric Lim <5cminsuhlim@gmail.com>
2023-01-20 17:38:56 -0500

    update setup

--- install_packages.R
+++
@@ -1,8 +0,0 @@
-install.packages('pacman', repos = 'https://cran.rstudio.com')
-
-library(pacman)
-
-pkgs <- readLines('packages.csv')
-p_load(char = setdiff(pkgs, c('icons','emo')))
-p_install_gh('mitchelloharawild/icons')
-p_install_gh('hadley/emo')
